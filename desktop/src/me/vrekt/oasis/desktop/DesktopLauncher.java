d97b4e6111d806d47e72aaa146d17f50c4bdd055
Vrekt <Vrekt@users.noreply.github.com>
2021-10-05 23:53:17 -0500

    Add desktop

---
+++ desktop/src/me/vrekt/oasis/desktop/DesktopLauncher.java
@@ -0,0 +1,21 @@
+package me.vrekt.oasis.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import me.vrekt.oasis.OasisGame;
+import me.vrekt.oasis.settings.GameSettings;
+
+public class DesktopLauncher {
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+        config.vSyncEnabled = GameSettings.USE_VSYNC;
+        config.useGL30 = GameSettings.IS_GL_30;
+        config.pauseWhenBackground = GameSettings.PAUSE_IN_BACKGROUND;
+        config.pauseWhenMinimized = GameSettings.PAUSE_IN_BACKGROUND;
+        config.height = 600;
+        config.width = 800;
+
+        new LwjglApplication(new OasisGame(), config);
+    }
+}
