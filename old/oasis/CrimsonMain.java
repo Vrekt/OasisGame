37b9cf671374b80c2c1ba7c5404cf893d764a755
Vrekt <Vrekt@users.noreply.github.com>
2024-05-31 16:39:47 -0500

    Delete old/oasis directory

--- old/oasis/CrimsonMain.java
+++
@@ -1,9 +0,0 @@
-package me.vrekt.oasis;
-
-public class CrimsonMain {
-
-    public static void main(String[] args) {
-        new Crimson(args);
-    }
-
-}
--- old/oasis/world/CrimsonWorldManager.java
+++
@@ -1,9 +0,0 @@
-package me.vrekt.oasis.world;
-
-import gdx.lunar.server.world.impl.WorldManager;
-
-/**
- * Manages game worlds within Crimson
- */
-public final class CrimsonWorldManager extends WorldManager {
-}
